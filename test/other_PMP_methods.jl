82172da34048500186790e2af8503c533844697c
Anne Martin <anne.martin@polymtl.ca>
2023-06-23 17:18:12 -0400

    test PMP_Hershfield

--- test/other_PMP_methods.jl
+++ test/other_PMP_methods.jl
@@ -1,8 +1,8 @@
 
 @testset "Other PMP methods" begin
     
-    rain = load("data/mm_rain_data.jld2", "Rain")
-    date = load("data/mm_rain_data.jld2", "Date")
+    rain = load("test/data/mm_rain_data.jld2", "Rain")
+    date = load("test/data/mm_rain_data.jld2", "Date")
 
     # GEV
     @testset "PMP_GEV" begin
@@ -11,7 +11,7 @@
 
     # Hershfield
     @testset "PMP_Hershfield" begin        
-        @test PMP.PMP_Hershfield(rain, date, 15) == 265.8582209143689
+        @test PMP.PMP_Hershfield(rain, date, 15) ≈ 265.85822091436887
         @test PMP.PMP_Hershfield(rain, date)[1] == 84.41003515317207
     end
 
