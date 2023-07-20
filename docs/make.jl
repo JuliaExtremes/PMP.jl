79cca2de20b1a20e70509098da7f0cfd53b62704
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2023-07-20 10:18:25 -0400

    add documention structure

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Documenter, PMP
+
+CI = get(ENV, "CI", nothing) == "true"
+
+makedocs(sitename = "PMP.jl",
+    format = Documenter.HTML(
+    prettyurls = CI,
+    ),
+    pages = [
+       "GettingStarted.md",
+       "MoistureMaximization.md",
+       "index.md"]
+)
+
+if CI
+    deploydocs(
+    repo   = "github.com/JuliaExtremes/PMP.jl.git",
+    devbranch = "dev",
+    versions = ["stable" => "v^", "v#.#", "main"],
+    push_preview = false,
+    target = "build"
+    )
+end
---
+++ docs/src/MoistureMaximization.md
@@ -0,0 +1,8 @@
+
+# PMP estimation by moisture maximization 
+
+Blah blah blah
+
+## Subsection 1
+
+## Subsection 2
